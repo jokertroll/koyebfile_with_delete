30997a26f07690ee5dbc22306ed1c7bb4b601231
jokertroll <108746561+jokertroll@users.noreply.github.com>
2025-07-21 22:01:49 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -10,7 +10,7 @@ from helper_func import encode
 # 🔹 Handles private messages from admin (to create post + generate shareable link)
 admin_commands = ['start', 'users', 'broadcast', 'batch', 'genlink', 'stats', 'join', 'try_premium',
                   'music', 'premium', 'addpremium', 'listpremium', 'mypremium', 'extendpremium',
-                  'revokepremium', 'trackPromo', 'generatePromo', 'resetTrial','adminHelp']
+                  'revokepremium', 'trackPromo', 'generatePromo', 'resetTrial','adminHelp','put']
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(admin_commands))
 async def channel_post(client: Client, message: Message):
